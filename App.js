6a567b14aade69e05916e215e6e4724e7f3163b3
Ddvrg <69285654+Ddvrg@users.noreply.github.com>
2020-10-22 11:38:02 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import React, { Component } from "react";
+import { View } from "react-native";
+import LoginScreen from "./screens/LoginScreen";
+import HomeScreen from "./screens/HomeScreen"
+import MainScreen from "./screens/Main";
+
+const Stack = createStackNavigator();
+export default class App extends Component{
+  render(){
+    return(
+      <View style={{ flex:1 }}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='Home' component={HomeScreen} />
+          <Stack.Screen name='Login' component={LoginScreen} />
+          <Stack.Screen name='Main' component={MainScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </View>
+    )
+  }
+}
